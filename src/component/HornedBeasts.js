dab8202d6098444bec55a517b8be27a86d3d40d6
Ammar00Areeda <akiareeda@yahoo.com>
2021-09-12 19:59:02 +0300

    first time with react

---
+++ src/component/HornedBeasts.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class HornedBeasts extends React.Component{
+    constructor(title,imgurl,dicrption)
+    this.title=title;
+    this.imgurl=imgurl;
+    this.dicrption=dicrption;
+
+
+}
+
+export default HornedBeasts;
---
+++ src/component/Main.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+
+class Main extends HornedBeasts, React.Component{
+    constructor (title,imgurl,dicrption)
+    super (title,imgurl,dicrption)
+
+    render(){
+        <div>
+            {/* object  */}
+        </div>
+    }
+
+    
+
+    
+
+
+}
